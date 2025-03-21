48a9a88b948b7cb382445c66b7e0106074031be3
bezmari19 <marina.bezerraf19@gmail.com>
2025-03-21 08:45:12 -0300

    commit feito

---
+++ Aulas/Site.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html> 
+<body>
+    <h1>
+        Meu primeiro Site em JavaScript
+    </h1>
+    <buttom type="buttom" onclick="document.getElementById('demo').innerHTML = Image()">
+    Clique aqui e veja a imagem!</buttom>
+</body>
